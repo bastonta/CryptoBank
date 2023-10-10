fe23c9d511f17ab45e72a8094d35466fc8446ad1
Abylay <8089119+bastonta@users.noreply.github.com>
2023-10-10 13:03:07 +0600

    Add accounts listing feature

---
+++ src/CryptoBank.WebApi/Features/Account/Requests/GetAccounts.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Claims;
+using CryptoBank.WebApi.Data;
+using FastEndpoints;
+using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoBank.WebApi.Features.Account.Requests;
+
+public static class GetAccounts
+{
+    [HttpGet("/account")]
+    public class Endpoint : Endpoint<Request, Response[]>
+    {
+        private readonly IMediator _mediator;
+
+        public Endpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override async Task<Response[]> ExecuteAsync(Request req, CancellationToken cancellationToken) =>
+            await _mediator.Send(req, cancellationToken);
+    }
+
+    public record Request(
+        [property: FromClaim(ClaimTypes.NameIdentifier)] Guid UserId
+    ) : IRequest<Response[]>;
+
+    public record Response(
+        string Number,
+        string Currency,
+        decimal Amount,
+        DateTime DateOfOpening
+    );
+
+    public class RequestHandler : IRequestHandler<Request, Response[]>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public RequestHandler(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async ValueTask<Response[]> Handle(Request request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Accounts
+                .Where(s => s.UserId == request.UserId)
+                .Select(s => new Response(s.Number, s.Currency, s.Amount, s.DateOfOpening))
+                .ToArrayAsync(cancellationToken);
+        }
+    }
+}
