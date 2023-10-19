61aee4fbe1e097573a581efa2ce06835df0dec0e
Abylay <8089119+bastonta@users.noreply.github.com>
2023-10-19 14:26:43 +0600

    Add GetAccountsTests for account feature (#27)

--- tests/CryptoBank.WebApi.Tests.Integration/Helpers/HttpClientExtensions.cs
+++ tests/CryptoBank.WebApi.Tests.Integration/Helpers/HttpClientExtensions.cs
@@ -6,12 +6,26 @@ namespace CryptoBank.WebApi.Tests.Integration.Helpers;
 
 public static class HttpClientExtensions
 {
+    public static async Task<TResponse> GetAsJsonAsync<TResponse>(this HttpClient client, string url,
+        HttpStatusCode expectedStatus = HttpStatusCode.OK)
+    {
+        var httpResponse = await client.GetAsync(url);
+        httpResponse.StatusCode.Should().Be(expectedStatus);
+
+        return await httpResponse.DeserializeResponse<TResponse>();
+    }
+
     public static async Task<TResponse> PostAsJsonAsync<TResponse>(this HttpClient client, string url, object? body,
         HttpStatusCode expectedStatus = HttpStatusCode.OK)
     {
         var httpResponse = await client.PostAsync(url, JsonContent.Create(body));
         httpResponse.StatusCode.Should().Be(expectedStatus);
 
+        return await httpResponse.DeserializeResponse<TResponse>();
+    }
+
+    private static async Task<TResponse> DeserializeResponse<TResponse>(this HttpResponseMessage httpResponse)
+    {
         var responseString = await httpResponse.Content.ReadAsStringAsync();
         return JsonSerializer.Deserialize<TResponse>(responseString, new JsonSerializerOptions
         {
