05960227b0b142b799b5613de2771dd5a2699c0d
Abylay <8089119+bastonta@users.noreply.github.com>
2023-10-24 19:34:27 +0600

    Account transfers tests (#29)

---
+++ tests/CryptoBank.WebApi.Tests.Integration/Errors/Contracts/ValidationErrorProblemDetailsContract.cs
@@ -0,0 +1,22 @@
+﻿namespace CryptoBank.WebApi.Tests.Integration.Errors.Contracts;
+
+public class ValidationErrorProblemDetailsContract
+{
+    public string Title { get; set; }
+
+    public string Type { get; set; }
+
+    public string Detail { get; set; }
+
+    public int Status { get; set; }
+
+    public string TraceId { get; set; }
+
+    public ErrorData[] Errors { get; set; }
+}
+
+public record ErrorData(
+    string Field,
+    string Message,
+    string Code
+);
