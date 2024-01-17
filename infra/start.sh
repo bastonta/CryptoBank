4b42733537f0458ec125770f3c67742f56576519
Abylay <8089119+bastonta@users.noreply.github.com>
2024-01-17 18:07:24 +0600

    Add deploy webapi (#39)

--- infra/start.sh
+++ infra/start.sh
@@ -5,17 +5,46 @@ terraform -chdir=terraform apply
 terraform -chdir=terraform output | awk '{ gsub (" ", "", $0); print}' > .env
 
 # Export environment variables
+postgres_user=postgres
+postgres_password=qwerty
 export $(cat .env | xargs)
 
 # Create ansible inventory
-echo "[database]" > ansible/inventory.ini
-echo "$database_public_ip" >> ansible/inventory.ini
-echo "" >> ansible/inventory.ini
-echo "[database:vars]" >> ansible/inventory.ini
-echo "ansible_user=root" >> ansible/inventory.ini
-echo "database_ip=$database_ip" >> ansible/inventory.ini
-echo "backend_ip=$backend_ip" >> ansible/inventory.ini
-echo "database_volume_id=$database_volume_id" >> ansible/inventory.ini
+echo "" > ansible/inventory.ini
+{
+  echo "[frontend]"
+  echo "$frontend_public_ip"
+  echo ""
+
+  echo "[frontend:vars]"
+  echo "ansible_user=root"
+  echo "backend_ip=$backend_ip"
+  echo ""
+
+  echo "[backend]"
+  echo "$backend_public_ip"
+  echo ""
+
+  echo "[backend:vars]"
+  echo "ansible_user=root"
+  echo "database_ip=$database_ip"
+  echo "postgres_user=$postgres_user"
+  echo "postgres_password=$postgres_password"
+  echo ""
+
+  echo "[database]"
+  echo "$database_public_ip"
+  echo ""
+
+  echo "[database:vars]"
+  echo "ansible_user=root"
+  echo "database_ip=$database_ip"
+  echo "backend_ip=$backend_ip"
+  echo "database_volume_id=$database_volume_id"
+  echo "postgres_user=$postgres_user"
+  echo "postgres_password=$postgres_password"
+  echo ""
+} >> ansible/inventory.ini
 
 # Start ansible
 ansible-playbook -i ansible/inventory.ini ansible/playbook.yml
